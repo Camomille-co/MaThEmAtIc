712f776f6dd650d0d3499d85c31351a31147f92d
cazaz <cazaz@DESKTOP-RR0CBRF>
2024-02-05 21:49:34 +0800

    Добавьте файлы проекта.

---
+++ MaThEmAtIc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MaThEmAtIc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
